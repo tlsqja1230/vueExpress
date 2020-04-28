187f2c66adea88631c29c10ec62b60765eef3d0e
shinbum.kang <tlsqja1230@naver.com>
2020-04-29 00:13:08 +0900

    vuetify framework 추가, 기본템플릿 추가

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,14 +1,17 @@
-module.exports = { 
-    devServer: {
-      proxy: { 
-        '/api': { 
-          target: 'http://localhost:3000/api',
-          changeOrigin: true, 
-          pathRewrite: { 
-            '^/api': ''
-          } 
-        } 
-      } 
-    },
-    outputDir: '../back/public',
+module.exports = {
+  "devServer": {
+    "proxy": {
+      "/api": {
+        "target": "http://localhost:3000/api",
+        "changeOrigin": true,
+        "pathRewrite": {
+          "^/api": ""
+        }
+      }
+    }
+  },
+  "outputDir": "../back/public",
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
